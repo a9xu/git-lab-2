fdbedf0ed477becc95dbd720921d8a10061ed089
a9xu <o.gharaybeh1@gmail.com>
2023-09-08 17:40:31 -0400

    a

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+*      File: git-lab-program.cc
+*      Author: Omar Gharaybeh
+*      Date: Friday, September 8th, 2023
+*Description: Lab2 git
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!" << endl;
+    return 0;
+
+}// main
